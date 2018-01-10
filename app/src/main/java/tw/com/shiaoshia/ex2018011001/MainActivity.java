1d37796628dec5e9c88fbd3778ba383b5e075b73
shiaoshia <34782716+shiaoshia@users.noreply.github.com>
2018-01-10 10:57:44 +0800

    handler = new Handler(); 數秒

--- app/src/main/java/tw/com/shiaoshia/ex2018011001/MainActivity.java
+++ app/src/main/java/tw/com/shiaoshia/ex2018011001/MainActivity.java
@@ -1,13 +1,44 @@
 package tw.com.shiaoshia.ex2018011001;
 
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Handler handler;
+    TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        handler = new Handler();
+        tv = findViewById(R.id.textView);
+        MyThread t = new MyThread();
+        t.start();
     }
+
+    class MyThread extends Thread {
+        @Override
+        public void run() {
+            super.run();
+            while (true) {
+                try {
+                    Thread.sleep(1000); //延遲1秒
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                //handler.post(new Runable());
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        tv.setText(String.valueOf(Integer.valueOf(tv.getText().toString()) + 1));
+                    }
+                });
+            }
+        }
+    }
+
 }
